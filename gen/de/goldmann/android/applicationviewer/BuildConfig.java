47d3dd50c46ee0fb764a7f175bf5d49fed077c1c
Michael Goldmann <goldmann.michael@googlemail.com>
2012-12-12 09:54:09 +0100

    initial project setup

---
+++ gen/de/goldmann/android/applicationviewer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.goldmann.android.applicationviewer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
